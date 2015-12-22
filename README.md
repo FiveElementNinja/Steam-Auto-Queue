87f213c6698f32e0a2e930a00a61b98fb29e0f8f
Five Element Ninja <code@fiveelementninja.com>
2015-12-22 15:29:06 -0600

    Initial code commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Steam-Auto-Queue
 This script will automatically click next on all the games in your queue so you can clear it out and get your card.
+
+I recommend disabling this script once you have received your 3 cards for the day. Otherwise it may kick in when you're trying to view the store page for a game.
---
+++ steam-auto-queue.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Steam Auto Queue
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This script will automatically click next on all the games in your queue so you can clear it out and get your card.
+// @author       Five Element Ninja (Twitter: @FivElementNinja)
+// @match        *://store.steampowered.com/app/*
+// @updateURL    https://github.com/FiveElementNinja/steam-auto-queue/steam-auto-queue.js
+// @downloadURL  https://github.com/FiveElementNinja/steam-auto-queue/steam-auto-queue.js
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+console.log('Steam Auto Queue Loaded - Will try to click Next automatically...');
+try {
+	$J('#next_in_queue_form').submit();
+}
+catch(err) {
+	console.log('Error clicking next.');
+}
