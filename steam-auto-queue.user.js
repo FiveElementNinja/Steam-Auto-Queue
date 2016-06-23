9e3eb8aaea8ebc82d5714ec1fadd0f683b94c555
Five Element Ninja <code@fiveelementninja.com>
2016-06-23 13:54:14 -0500

    Updated the description and version number to try and make Tampermonkey not think this is a downgrade.

--- steam-auto-queue.user.js
+++ steam-auto-queue.user.js
@@ -2,7 +2,7 @@
 // @name         Steam Auto Queue
 // @namespace    https://github.com/FiveElementNinja/Steam-Auto-Queue/
 // @version      1.0
-// @description  This script will automatically click next on all the games in your queue so you can clear it out and get your card.
+// @description  This script will automatically click through your discovery queue so you can get a sale card.
 // @author       Five Element Ninja (Twitter: @FivElementNinja)
 // @match        *://store.steampowered.com/app/*
 // @updateURL    https://raw.githubusercontent.com/FiveElementNinja/Steam-Auto-Queue/master/steam-auto-queue.user.js
