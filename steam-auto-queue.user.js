9f0fee073dde1ce8ca962fcf8ff14b27c98b1b2d
Vladislav <Kasp.KasperLab@yandex.ru>
2016-06-24 11:18:31 +0300

    Update steam-auto-queue.user.js

--- steam-auto-queue.user.js
+++ steam-auto-queue.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Auto Queue
 // @namespace    https://github.com/FiveElementNinja/Steam-Auto-Queue/
-// @version      1.5
+// @version      1.6
 // @description  This script will automatically click through your discovery queue so you can get a sale card.
 // @author       Five Element Ninja (Twitter: @FivElementNinja)
 // @match        *://store.steampowered.com/app/*
@@ -17,11 +17,13 @@
     console.log('Steam Auto Queue Loaded - Will try to click Next automatically...');
 	try {
         if (window.location.pathname.indexOf('/agecheck') > -1) {
+			var appid = window.location.pathname.replace('/agecheck', '').replace('/app/', '');
             var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
             $J('#agecheck_form select[name="ageDay"] option[value="'+rand(1,29)+'"]').attr('selected','selected');
             $J('#agecheck_form select[name="ageYear"] option[value="'+rand(1985,1995)+'"]').attr('selected','selected');
             $J('#agecheck_form select[name="ageMonth"] option[value="'+month[rand(0,11)]+'"]').attr('selected','selected');
             $J('#agecheck_form').submit();
+			HideAgeGate(appid);
         } else {
             $J('#next_in_queue_form').submit();
         }
